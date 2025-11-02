1a9a22a60f59fd293f2c7d2c77db574d433b9914
inprovert <92786046+inprovert@users.noreply.github.com>
2025-11-02 17:52:13 +0530

    nav bug

--- app/page.tsx
+++ app/page.tsx
@@ -9,9 +9,10 @@ export default function Home() {
     <div className="bg-[#0E0915]  flex h-screen flex-col justify-between">
        <Image src={"/fade_gradient.svg"} alt = 'broo' fill className="absolute inset-0 w-screen h-screen object-cover" />
         <nav className="flex justify-between items-center  m-2 p-2 border-b border-white border-dashed border-neutral ">
-       <div className="flex flex-row justify-start items-center mr-1"> <Image src={"/acl.png"} width={50} height={10} alt="logo" />
-          <Link href="/"> <h1 className="text-xl font-bold mr-2 text-white">ACL2025</h1> </Link> </div>
-           <p className="text-sm p-1 text-white">Made with ♥ by <Link className="text-blue-400" href="https://x.com/0xPranavx">2Players</Link> 
+       <div className="flex flex-row justify-start items-center mr-1 z-10"> <Image src={"/acl.png"} width={50} height={10} alt="logo" />
+          {/* <Link href="/"> <h1 className="text-xl font-bold mr-2 text-white">ACL2025</h1> </Link>  */}
+          </div>
+           <p className="text-sm p-1 text-white z-10">Made with ♥ by <Link className="text-blue-400 z-10" href="https://x.com/0xPranavx">2Players</Link> 
        . </p>
       </nav> 
       
